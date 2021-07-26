c339888cf883e793ceeebbedd28271b22502e0e6
ghostfacedlabs <ghostfacedlabs@gmail.com>
2021-07-26 11:16:36 -0700

    V1.0 application upload, functionally working

--- app/src/main/java/com/example/bin2dec/MainActivity.java
+++ app/src/main/java/com/example/bin2dec/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.bin2dec;
 
+import android.annotation.SuppressLint;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button convertBinary = findViewById(R.id.convertBinary);
+        Button convertDecimal = findViewById(R.id.convertDecimal);
+        Button clearButton = findViewById(R.id.reset);
+        EditText binaryView = findViewById(R.id.binTextEntry);
+        TextView decimalView = findViewById(R.id.decimalText);
+
+        convertBinary.setOnClickListener(view -> {
+            String value = binaryView.getText().toString();
+
+            if (value.isEmpty()) {
+                binaryView.setError("Enter a valid Binary Number");
+            }
+            try {
+                decimalView.setText(Integer.toString(Integer.parseInt(value, 2)));
+
+            } catch (NumberFormatException n) {
+                binaryView.setError("Enter a valid Binary Number");
+
+            }
+        });
+
+        convertDecimal.setOnClickListener(view -> {
+            String value = decimalView.getText().toString();
+
+
+            if (value.isEmpty()) {
+                decimalView.setError("Enter a valid Integer Number");
+            }
+
+            try {
+                binaryView.setText(Integer.toBinaryString(Integer.parseInt(value)));
+
+            } catch (Exception e) {
+                decimalView.setError("Enter a valid Integer Number");
+            }
+
+
+        });
+
+        clearButton.setOnClickListener(view -> {
+            decimalView.setText("");
+            binaryView.setText("");
+        });
+
     }
 }
